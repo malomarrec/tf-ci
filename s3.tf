8722b9994bf1cc08c49075c02151c7246061b5a5
malomarrec <malo.marrec@gmail.com>
2021-02-18 16:37:04 +0100

    add s3

---
+++ s3.tf
@@ -0,0 +1,34 @@
+terraform {
+  backend "remote" {
+    organization = "malomarrec"
+
+    workspaces {
+      name = "malomito"
+    }
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-efrfw-132454231"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
